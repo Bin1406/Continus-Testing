92165306e6b80ff316b1486144dc295824b3e578
Bin1406 <63855334+Bin1406@users.noreply.github.com>
2022-06-01 19:07:45 +0700

    Update Driver.java

--- Basic-Java-Maven-main/src/main/java/Driver.java
+++ Basic-Java-Maven-main/src/main/java/Driver.java
@@ -5,16 +5,19 @@ public class Driver {
 	public static void main(String[] args) {
 		
 		Counter counter = new Counter();
-		
+
 		System.out.println("Count:" + counter.getCount());
+		System.out.println("Current Count:" + counter.getCount());
 		
 		counter.increment();
 		
 		System.out.println("Count:" + counter.getCount());
+		System.out.println("Current Count:" + counter.getCount());
 		
 		counter.decrement();
 		
 		System.out.println("Count:" + counter.getCount());
+		System.out.println("Current Count:" + counter.getCount());
 		
 	}
 
